0f39e13b95eac57d71fa3d825b364a7bc0f5cac5
Anssi Kesänen <anssi.kesanen@gmail.com>
2021-03-04 17:28:37 +0200

    Added volume

--- blocks.h
+++ blocks.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"batterystatus",			5,			0},
+	{"",		"sb-volume",				0,		       10},
 
-	{"", 		"clock",				20,			0},
+	{"",		"sb-batterystatus",			5,			0},
 
-	{"",		"updatechecker",		      1800,			0},
+	{"", 		"sb-clock",				20,			0},
+
+	{"",		"sb-updatechecker",		      1800,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
